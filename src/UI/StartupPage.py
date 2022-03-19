cefd3063de03e0e66515c4111474be6af50f1049
achyuthparth <achyuthaa@outlook.com>
2022-03-19 12:17:48 -0700

    starting ui coding

---
+++ src/UI/StartupPage.py
@@ -0,0 +1,8 @@
+from tkinter import *
+
+practiceButton = Button(root, text="Practice", command=myClick)
+
+enteringButton = Button(root, text = "Enter New Set", command = myClick)
+
+progressButton = Button(root, text = "View Progress Report", command = myClick)
+
